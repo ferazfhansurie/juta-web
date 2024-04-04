bdfaa965e66f4d48258b1c2b717ed28c969dedaf
ferazfhansurie <ferazfhansurie@gmail.com>
2024-04-04 16:26:05 +0800

    Add firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,12 @@
 import admin from 'firebase-admin';
 const firebaseConfig = {
-    apiKey: "AIzaSyCc0oSHlqlX7fLeqqonODsOIC3XA8NI7hc",
-    authDomain: "onboarding-a5fcb.firebaseapp.com",
-    databaseURL: "https://onboarding-a5fcb-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "onboarding-a5fcb",
-    storageBucket: "onboarding-a5fcb.appspot.com",
-    messagingSenderId: "334607574757",
-    appId: "1:334607574757:web:2603a69bf85f4a1e87960c",
-    measurementId: "G-2C9J1RY67L"
+  apiKey: "AIzaSyDOijfOd2G2SnKcOejWPJt2qyzDYe8gYYM",
+  authDomain: "tatapies.firebaseapp.com",
+  projectId: "tatapies",
+  storageBucket: "tatapies.appspot.com",
+  messagingSenderId: "153923200089",
+  appId: "1:153923200089:web:e394f993c09c8c20d9bd48",
+  measurementId: "G-5BGBRYHGLQ"
   };
 // Check if we haven't initialized it yet
 admin.initializeApp({
